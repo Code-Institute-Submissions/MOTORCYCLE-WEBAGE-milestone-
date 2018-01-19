137a9066c168000afdae5486e47d2948827ba025
Lorna Treagus <Lorna@Lornas-MacBook-Pro.local>
2018-01-19 07:52:20 +0000

    started making main page responsive

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
 
 // google maps
 function initMap() {
+
     var uluru = { lat: 50.8599851, lng: -0.9675234000000046 };
+    
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: uluru
@@ -11,4 +13,3 @@ function initMap() {
         map: map
     });
 }
-initMap();
